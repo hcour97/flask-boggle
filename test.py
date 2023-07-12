d1c39b22dc40e92baa51f4592607d0c31de44303
Hannah <hcour@gmail.com>
2023-07-12 06:45:00 -0400

    updating test file

--- test.py
+++ test.py
@@ -6,5 +6,50 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        """Things to do before every test."""
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+    
+    def test_homepage_start(self):
+        """Test homepage loads: make sure info is in the session and HTML is displayed"""
+
+        with self.client:
+            response = self.client.get("/")
+            self.assertIn('board', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertIsNone(session.get('nplays'))
+            self.assertIn(b'<p>High Score:', response.data)
+            self.assertIn(b'Score:', response.data)
+            self.assertIn(b'Seconds Left:', response.data)
+    
+    def test_homepage(self):
+        """Test if the word is valid by modifying the board in the session"""
+
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"]]
+                response = self.client.get('/board/check?word=cat')
+                self.assertEqual(response.json['result'], 'ok')
+    
+    def test_invalid_word(self):
+        """Test if word is in dictionary"""
+
+        self.client.get('/')
+        response = self.client.get('/board/check?word=impossible')
+        self.assertEqual(response.json['result'], 'not-on-board')
+
+    
+    def non_english_word(self):
+        """Test if word is on the board"""
+
+        self.client.get('/')
+        response = self.client.get('/board/check?word=ydfahuigj')
+        self.assertEqual(response.json['result'], 'not-word')
+
+
 
